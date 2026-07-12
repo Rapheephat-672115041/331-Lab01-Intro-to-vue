d55eca716b5add0ea3304b61c1fd778116b37fe3
Rapheephat <rapheephat_a@cmu.ac.th>
2026-07-12 17:21:07 +0700

    checkpoint 4.9
    Changed productDescription to h3 and add sales text with condition

--- main.js
+++ main.js
@@ -6,12 +6,18 @@ createApp({
         const productDescription = ref('You wear it when wearing studs')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th/')
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const onSale = ref(true)
 
         return {
             product,
             productDescription,
             image,
-            link
+            link,
+            inStock,
+            inventory,
+            onSale
         }
     }
 }).mount('#app')
