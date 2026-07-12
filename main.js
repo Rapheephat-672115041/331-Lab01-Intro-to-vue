a90e5825105d6c9d33f0629c03b52fd8a5b620e7
Rapheephat <rapheephat_a@cmu.ac.th>
2026-07-12 17:09:23 +0700

    checkpoint 3.6
    so its really socks? then why did you make me change it to boots and never tell me to change it back to socks lol. it wasnt a big deal tho, just a little nitpick thing haha.

--- main.js
+++ main.js
@@ -2,12 +2,16 @@ const {createApp, ref} = Vue
 
 createApp({
     setup(){
-        const product = ref('Boots')
-        const productDescription = ref('You wear it in mud, or in rainy day')
+        const product = ref('Socks')
+        const productDescription = ref('You wear it when wearing studs')
+        const image = ref('./assets/images/socks_green.jpg')
+        const link = ref('https://www.camt.cmu.ac.th/')
 
         return {
             product,
-            productDescription
+            productDescription,
+            image,
+            link
         }
     }
 }).mount('#app')
