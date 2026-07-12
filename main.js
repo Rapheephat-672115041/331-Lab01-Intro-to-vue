6d84a17685fe7494381a4c3b7abc9447273becca
Rapheephat <rapheephat_a@cmu.ac.th>
2026-07-12 18:13:45 +0700

    checkpoint 6.7
    Added toggle button for inStock status, and also add inStock into their elements as it wasnt on all of them yet. Added other functionalities in the same checkpoint

--- main.js
+++ main.js
@@ -15,12 +15,25 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            {id: 2234, color: 'green'},
-            {id: 2235, color: 'blue'}
+            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
         ])
         const sizes = ref([
             'S', 'M', 'L'
         ])
+        const cart = ref(0)
+
+        function addToCart() {
+            cart.value +=1
+        }
+
+        function updateImage(variantImage) {
+            image.value = variantImage
+        }
+
+        function toggleStockStatus() {
+            inStock.value = !inStock.value
+        }
 
         return {
             product,
@@ -32,7 +45,11 @@ createApp({
             onSale,
             details,
             variants,
-            sizes
+            sizes,
+            cart,
+            addToCart,
+            updateImage,
+            toggleStockStatus
         }
     }
 }).mount('#app')
