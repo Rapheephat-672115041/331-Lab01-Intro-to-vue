33dddfe7ebfe3b3c06a83023781cf23c0d2513ad
Rapheephat <rapheephat_a@cmu.ac.th>
2026-07-12 20:13:32 +0700

    checkpoint 9.9
    WHAT. Prof, tell me we're using component folder like this in future lab so we dont need to migrate our codes like this again. I know you want to show us the thing about Vue and stuffs, but damn this is pretty rough haha...

---
+++ components/productDisplay.js
@@ -0,0 +1,122 @@
+const productDisplay = {
+  template:
+    /*html*/
+    `
+    <div class="product-display">
+            <div class="product-container">
+                <div class="product-image">
+                    <img :src="image" :class="{'out-of-stock-image': !inStock}">
+                </div>
+            </div>
+        </div>
+        <div class="product-info">
+            <h1>
+                <a :href="link">{{title}}</a>
+            </h1>
+            <h2 v-if="onSale">{{displaySale}}</h2>
+            <h3>{{productDescription}}</h3>
+            <p v-if="inventory > 10 && inStock">In stock</p>
+            <p>Shipping: {{shipping}}</p>
+            <p v-else-if="inventory <= 10 && inventory > 0 && inStock">Almost out of stock</p>
+            <p v-else>Out of stock</p>
+            <ul>
+                <li v-for="detail in details">{{detail}}</li>
+            </ul>
+            <div v-for="(variant,index) in variants" :key="variant.id"
+            @mouseover="updateVariant(index)"
+            class="color-circle" :style="{backgroundColor: variant.color}">
+            </div>
+            <ul class="sizeList">
+                <li v-for="size in sizes">{{size}}</li>
+            </ul>
+            <button class="button" :disabled='!inStock' @click="addToCart" :class="{disabledButton: !inStock}">Add to cart</button>
+            <button class="button" @click="toggleSale">Toggle Sale</button>
+        </div>
+        `,
+        props: {
+            premium: Boolean
+        },
+  setup(props) {
+    const product = ref("Socks");
+    const brand = ref("SE 331");
+    const productDescription = ref("You wear it when wearing studs");
+    const link = ref("https://www.camt.cmu.ac.th/");
+    const inventory = ref(100);
+    const onSale = ref(true);
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+    const variants = ref([
+      {
+        id: 2234,
+        color: "green",
+        image: "./assets/images/socks_green.jpg",
+        quantity: 50,
+      },
+      {
+        id: 2235,
+        color: "blue",
+        image: "./assets/images/socks_blue.jpg",
+        quantity: 0,
+      },
+    ]);
+    const selectedVariant = ref(0);
+    const sizes = ref(["S", "M", "L"]);
+
+    function addToCart() {
+      cart.value += 1;
+    }
+
+    const title = computed(() => {
+      return brand.value + " " + product.value;
+    });
+
+    function updateVariant(index) {
+      selectedVariant.value = index;
+    }
+
+    const image = computed(() => {
+      return variants.value[selectedVariant.value].image;
+    });
+
+    const inStock = computed(() => {
+      return variants.value[selectedVariant.value].quantity;
+    });
+
+    const displaySale = computed(() => {
+      if (onSale.value) {
+        return brand.value + " " + product.value + " is on sale!";
+      }
+
+      return "";
+    });
+
+    function toggleSale() {
+      onSale.value = !onSale.value;
+    }
+
+    const shipping = computed(() => {
+        if (props.premium) {
+            return 'Free'
+        } else {
+            return 30
+        }
+    })
+
+    return {
+      title,
+      productDescription,
+      image,
+      link,
+      inStock,
+      inventory,
+      onSale,
+      details,
+      variants,
+      addToCart,
+      sizes,
+      updateVariant,
+      displaySale,
+      toggleSale,
+      shipping
+    };
+  },
+};
--- main.js
+++ main.js
@@ -1,81 +1,14 @@
 const {createApp, ref, computed} = Vue
 
-createApp({
+const app = createApp({
     setup(){
-        const product = ref('Socks')
-        const brand = ref('SE 331')
-        const productDescription = ref('You wear it when wearing studs')
-        const link = ref('https://www.camt.cmu.ac.th/')
-        const inventory = ref(100)
-        const onSale = ref(true)
-        const details = ref([
-            '50% cotton',
-            '30% wool',
-            '20% polyester'
-        ])
-        const variants = ref([
-            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50},
-            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0}
-        ])
-        const selectedVariant = ref(0)
-        const sizes = ref([
-            'S', 'M', 'L'
-        ])
         const cart = ref(0)
-
-        function addToCart() {
-            cart.value +=1
-        }
-
-        function toggleStockStatus() {
-            inStock.value = !inStock.value
-        }
-
-        const title = computed(() => {
-            return brand.value  + ' ' + product.value
-        })
-
-        function updateVariant(index) {
-            selectedVariant.value = index;
-        }
-
-        const image = computed(() => {
-            return variants.value[selectedVariant.value].image
-        })
-
-        const inStock = computed(() => {
-            return variants.value[selectedVariant.value].quantity
-        })
-
-        const displaySale = computed(() => {
-            if (onSale.value) {
-                return brand.value + ' ' + product.value + ' is on sale!'
-            }
-
-            return ''
-        })
-
-        function toggleSale() {
-            onSale.value = !onSale.value
-        }
-
+        const premium = ref(true)
         return {
-            title,
-            productDescription,
-            image,
-            link,
-            inStock,
-            inventory,
-            onSale,
-            details,
-            variants,
-            sizes,
             cart,
-            addToCart,
-            updateVariant,
-            toggleStockStatus,
-            displaySale,
-            toggleSale
+            premium
         }
     }
-}).mount('#app')
+})
+app.component('product-display', productDisplay)
+app.mount('#app')
