8ae69046d796b4ef8b758d731db04cd02dc171b7
Rapheephat <rapheephat_a@cmu.ac.th>
2026-07-13 15:11:11 +0700

    checkpoint 9.10
    im not sure about product details components so i have nothing to say. sorry for that

---
+++ components/productDetails.js
@@ -0,0 +1,14 @@
+const productDetails = {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template: /*html*/
+  `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `
+};
